5e8d662b8ba64d8cec10895002516bb296299ad5
TM1744 <marquesthiago1744@gmail.com>
2025-06-19 11:33:30 -0300

    Cliente: dao-5

--- src/camadas/model/dao/ClienteDao.java
+++ src/camadas/model/dao/ClienteDao.java
@@ -5,6 +5,7 @@ import camadas.model.domain.Cliente;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteDao {
@@ -35,25 +36,67 @@ public class ClienteDao {
             throw new RuntimeException(e);
         }
     }
-    public Cliente get (String cod){
+    public Cliente get (String cod) throws RuntimeException{
         try{
             Database db = new Database();
             PreparedStatement stm = db.connection.prepareStatement("select * from clientes where cod = ?");
             stm.setString(1, cod);
             ResultSet result = stm.executeQuery();
+            stm.close();
+            result.close();
+            db.connection.close();
             return new Cliente(result.getString("nome"), result.getString("telefone"), result.getString("endereco"), result.getString("cod"));
         } catch (SQLException e){
             throw new RuntimeException(e);
         }
     }
-    public void getList(){
+    public List<Cliente> getList() throws RuntimeException{
+        try{
+            Database db = new Database();
+            ResultSet result = db.connection.createStatement().executeQuery("select * from clientes;");
+            List<Cliente> clientes = new ArrayList<>();
+            while (result.next()){
+                clientes.add(new Cliente(result.getString("nome"), result.getString("telefone"), result.getString("endereco"), result.getString("cod")));
+            }
+            result.close();
+            db.connection.close();
+            return clientes;
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
 
     }
-    public void update(Cliente cliente){
-
+    public void update(Cliente cliente) throws RuntimeException{
+        try{
+            Database db = new Database();
+            PreparedStatement stm = db.connection.prepareStatement("update clientes set nome = ?, telefone = ?, endereco = ? where cod = ?");
+            stm.setString(1, cliente.getNome());
+            stm.setString(2, cliente.getTelefone());
+            stm.setString(3, cliente.getEndereco());
+            stm.setString(4, cliente.getCod());
+            stm.executeUpdate();
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
-    public List<Cliente> getSearch(String nome){
+    public List<Cliente> getSearch(String nome) throws RuntimeException{
+        try{
+            Database db = new Database();
+            PreparedStatement stm = db.connection.prepareStatement("select * from clientes where nome like '%?%';");
+            stm.setString(1, nome);
+            ResultSet result = stm.executeQuery();
+            List<Cliente> clientes = new ArrayList<>();
+            while (result.next()){
+                clientes.add(new Cliente(result.getString("nome"), result.getString("telefone"), result.getString("endereco"), result.getString("cod")));
+            }
+            stm.close();
+            result.close();
+            db.connection.close();
+            return clientes;
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
 
     }
 }
